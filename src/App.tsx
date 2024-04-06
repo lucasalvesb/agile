ca7e5030f9e99f998b0191b8089e86513f961eaa
Lucas Alves <lucasalves94@gmail.com>
2024-04-05 23:15:32 -0300

    added basic header css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
+import Header from './components/layout/header'
 
 const App = () => {
   return (
-   <h1> hi </h1> 
+   <Header />
   )
 }
 
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,12 @@
+import Header from './header'
+import './styles.css'
+
+const Layout = () => {
+  return (
+    <div className='layout-container'>
+      <Header />
+    </div>
+  )
+}
+
+export default Layout
